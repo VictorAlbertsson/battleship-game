4c44f68e474a3bd9c00a13d9e1a82064d0b302c2
Victor Albertsson <victor.albertsson@live.se>
2020-12-17 10:48:43 +0100

    Auto-adjust graphical elements' position and size on window resize

--- main.scm
+++ main.scm
@@ -1,6 +1,3 @@
-#!/usr/bin/guile -s
-!#
-
 ;;; TODOs
 
 ;; DONE Render a single clickable UI element
@@ -39,20 +36,23 @@
   (tiles #:init-keyword #:tiles
 	 #:accessor     tiles))
 
+(define-class <player-board> (<board>))
+
+(define-class <enemy-board> (<board>))
+
 ;;; Global variables
 
 ;; Live coding setup
 (define repl (spawn-coop-repl-server))
 
-;; `tile' is temporary debug code
-(define tile-1 #f)
-(define tile-2 #f)
-(define tile-3 #f)
-
 ;; Global resources, set during load-time
-(define image  #f)
+(define image #f)
+
 (define width  #f)
 (define height #f)
+(define center #f)
+
+(define test #f)
 
 ;;; Helper functions
 
@@ -60,32 +60,71 @@
   (and (eq? button 'left)
        (rect-contains? button x y)))
 
-(define (draw-tile tile image x y)
-  (let ((width  (texture-width  image))
-	(height (texture-height image)))
-    (set! tile (rect x y width height))
-    (draw-sprite image (vec2 x y)
-		 #:origin (vec2 (/ width 2)
-				(/ height 2)))))
+;; Basic range function beacuse the Scheme standard is very minimal
+(define (range start stop step)
+  (if (or (and (>  step 0)
+               (>= start stop))
+          (and (<= step 0)
+               (<= start stop)))
+      '()
+      (cons start (range (+ start step) stop step))))
 
-;;; Core event-loop
+;; Index aware map function
+(define (map-range f start step . xs)
+  (let ((stop (+ start (* step (length (car xs))))))
+    (apply map (cons f
+                     (cons (range start stop step)
+                           xs)))))
+
+;; Draws a grid of images, WARNING: Assumes that all images are the
+;; same size!
+(define (draw-board images origin)
+  (define (f is xs)
+    (define (g i x)
+      (draw-sprite x (vec2+ origin
+			    (vec2* (vec2 (texture-width x) (texture-height x))
+				   (vec2 i                 is)))))
+    (map-range g 1 1 xs))
+  (map-range f 1 1 images))
+
+;;; Initialization
 
 (define (load)
-  (set! image  (load-image "./image.bmp")))
+  ;; Chickadee auto-adjusts the absolute position of sprites to
+  ;; accomodate for the window resizing, trying to center a sprite
+  ;; dynamically using the window width and height will fail since the
+  ;; position adjustment is done after the draw function is called.
+  ;; Therefore I store the the initial center of the window and use
+  ;; that to align graphical elements.
+  (set! center (vec2 (/ (window-width  (current-window)) 2)
+		     (/ (window-height (current-window)) 2)))
+  ;; TODO For testing purposes only!
+  (set! image  (load-image "./image.bmp"))
+  (set! test (list (list image image image)
+		   (list image image image)
+		   (list image image image))))
 
-(define (draw alpha)
-  #t
-  ;; (let* ((tile-1 #f)
-  ;; 	 (tile-2 #f)
-  ;; 	 (tile-3 #f))
-  ;;   (draw-tile tile-1 image))
-  )
+;;; Core event loop
 
 (define (update dt)
   (poll-coop-repl-server repl))
 
-(define (click mouse-button x y)
-  (cond ((button-clicked? tile-1 x y) (display "Gotcha!\n"))
-	(else #f)))
+(define (draw alpha)
+  (let ((width  (texture-width  image))
+	(height (texture-height image))
+	(left   (vec2 (/ (vec2-x center)       2) (vec2-y center)))
+	(right  (vec2 (/ (* 3 (vec2-x center)) 2) (vec2-y center))))
+    (draw-board test (vec2 0 0))
+    ;; (draw-sprite image left
+    ;; 		 #:origin (vec2 (/ width  2) (/ height 2)))
+    ;; (draw-sprite image right
+    ;; 		 #:origin (vec2 (/ width  2) (/ height 2)))
+    ))
 
-(run-game #:load load #:update update #:draw draw #:mouse-release click)
+(define (mouse-release mouse-button x y)
+  ;; (cond ((button-clicked? tile-1 x y) (display "Gotcha!\n"))
+  ;; 	(else #f))
+  #f
+  )
+
+(run-game #:load load #:update update #:draw draw #:mouse-release mouse-release)
